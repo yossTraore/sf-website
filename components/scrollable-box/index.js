4d9deb4f5d2e7742fe394d74a05ed989ca56ecb2
arzafran <franco@studiofreight.com>
2022-12-13 10:27:16 -0300

    fix imgs size

--- components/scrollable-box/index.js
+++ components/scrollable-box/index.js
@@ -9,10 +9,8 @@ export function ScrollableBox({ children, className, infinite }) {
   const [lenis, setLenis] = useState()
   const wrapperRef = useRef()
   const contentRef = useRef()
-  // const [setRef, rect] = useRect()
 
   useEffect(() => {
-    // window.scrollTo(0, 0)
     const lenis = new Lenis({
       wrapper: wrapperRef.current, // element which has overflow
       content: contentRef.current, // usually wrapper's direct child
@@ -22,7 +20,6 @@ export function ScrollableBox({ children, className, infinite }) {
       gestureDirection: 'vertical',
       smooth: true,
       infinite,
-      // infinite: true,
     })
 
     lenis.start()
@@ -37,19 +34,8 @@ export function ScrollableBox({ children, className, infinite }) {
     lenis?.raf(time)
   }, [])
 
-  // useEffect(() => {
-  //   console.log(rect)
-  // }, [rect])
-
   return (
-    <div
-      className={cn(s.hi, className)}
-      ref={(node) => {
-        // setRef(node)
-        wrapperRef.current = node
-      }}
-      style={{ '--max-height': '100px' }}
-    >
+    <div className={cn(s.hi, className)} ref={wrapperRef}>
       <div ref={contentRef}>{children}</div>
     </div>
   )
