bfa7cfc59552076c459c0db8dea49249bf826d64
arzafran <franco@studiofreight.com>
2022-12-15 20:06:31 -0300

    css only separator

--- components/separator/index.js
+++ components/separator/index.js
@@ -1,15 +1,14 @@
 import cn from 'clsx'
-import dynamic from 'next/dynamic'
 import s from './separator.module.scss'
 
-const Plus = dynamic(() => import('icons/plus.svg'), { ssr: false })
+// const Plus = dynamic(() => import('icons/plus.svg'), { ssr: false })
 
 export function Separator({ className }) {
   return (
     <div className={cn(s.separator, className)}>
-      <Plus className={s.icon} />
+      <div className={s.icon} />
       <span className={s.line} />
-      <Plus className={s.icon} />
+      <div className={s.icon} />
     </div>
   )
 }
