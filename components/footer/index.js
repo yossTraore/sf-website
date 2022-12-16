7d649fb13d8611ee6a8d2f273b98b9b4ff80cbe2
arzafran <franco@studiofreight.com>
2022-12-16 00:18:31 -0300

    mobile footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -13,48 +13,86 @@ export function Footer({ className, style, links, studioInfo }) {
       <Separator />
       <div className={cn(s.footer, 'layout-grid', className)} style={style}>
         <p className={cn(s.column, 'p-s text-muted')}>Built on Principle</p>
-        <ul className={s.column}>
-          {links.slice(0, 2).map((link, i) => (
-            <li key={i}>
-              <Link className="p-s decorate" href={link.url}>
-                {link.text}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <ul className={s.column}>
-          {links.slice(2, 4).map((link, i) => (
-            <li key={i}>
-              <Link className="p-s decorate" href={link.url}>
-                {link.text}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <ul className={s.column}>
-          {links.slice(4, 6).map((link, i) => (
-            <li key={i}>
-              <Link className="p-s decorate" href={link.url}>
-                {link.text}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        {isMobile === false && (
+          <>
+            <ul className={s.column}>
+              {links.slice(0, 2).map((link, i) => (
+                <li key={i}>
+                  <Link className="p-s decorate" href={link.url}>
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <ul className={s.column}>
+              {links.slice(2, 4).map((link, i) => (
+                <li key={i}>
+                  <Link className="p-s decorate" href={link.url}>
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <ul className={s.column}>
+              {links.slice(4, 6).map((link, i) => (
+                <li key={i}>
+                  <Link className="p-s decorate" href={link.url}>
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+
+        {isMobile === true && (
+          <>
+            <ul className={s.column}>
+              <li className="p-s text-muted">
+                &copy; {new Date().getFullYear()}
+              </li>
+            </ul>
+            <ul className={s.column}>
+              {links.slice(0, 3).map((link, i) => (
+                <li key={i}>
+                  <Link className="p-s decorate" href={link.url}>
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <ul className={s.column}>
+              {links.slice(3, 6).map((link, i) => (
+                <li key={i}>
+                  <Link className="p-s decorate" href={link.url}>
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+
         <ul className={s.column}>
           <li>
             <Link className="p-s decorate" href={`tel:${studioInfo.phone}`}>
-              {isMobile === false && 'P:'} {studioInfo.phone}
+              P: {studioInfo.phone}
             </Link>
           </li>
           <li>
             <Link className="p-s decorate" href={`mailto:${studioInfo.email}`}>
-              {isMobile === false && 'E:'} {studioInfo.email}
+              E: {studioInfo.email}
             </Link>
           </li>
         </ul>
-        <ul className={s.column}>
-          <li className="p-s text-muted">&copy; {new Date().getFullYear()}</li>
-        </ul>
+
+        {isMobile === false && (
+          <ul className={s.column}>
+            <li className="p-s text-muted">
+              &copy; {new Date().getFullYear()}
+            </li>
+          </ul>
+        )}
       </div>
 
       {isMobile === true && (
